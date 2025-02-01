d78df811f5217671c7a11907b3484dc165117487
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-01 17:34:04 +0100

    Fix problem with tags after migration

--- chrisblog-microservices_be/src/main/java/pl/kwi/chrisblog/db/repositories/TagRepository.java
+++ chrisblog-microservices_be/src/main/java/pl/kwi/chrisblog/db/repositories/TagRepository.java
@@ -10,7 +10,7 @@ import pl.kwi.chrisblog.db.entities.TagEntity;
 
 public interface TagRepository extends CrudRepository<TagEntity, Long> {
 	
-	@Query("SELECT t FROM TagEntity t JOIN t.articles a WHERE a.category.id = (:categoryId) GROUP BY t.name ORDER BY t.name ASC")
+	@Query("SELECT t FROM TagEntity t JOIN t.articles a WHERE a.category.id = (:categoryId) GROUP BY t.name, t.id ORDER BY t.name ASC")
 	public List<TagEntity> findAllByCategoryId(@Param("categoryId") long categoryId);
 	
 }
